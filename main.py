e84029ddb39096b8b128e51538afeb53cec54752
ori <ori.heldman@autobrains.ai>
2022-02-04 17:57:33 +0200

    dataframe for results analysis

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from pandas import json_normalize
 from tqdm import tqdm
 
 from board import Board
@@ -9,19 +10,19 @@ def simulate_games(first_player_class, second_player_class):
     depths = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
     results = []
-    for player_1_depth in depths:
-        for player_2_depth in tqdm(depths):
+    for player_1_depth in tqdm(depths):
+        for player_2_depth in depths:
             player_1 = first_player_class(player_1_depth, True)
             player_2 = second_player_class(player_2_depth, False)
 
             game = Game(Board(), player_1, player_2)
             winner, num_moves, player_1_times, player_2_times = game.simulateLocalGame()
-            results.append({'first_player': {'id': player_1.__class__,
+            results.append({'first_player': {'id': player_1.__class__.__name__,
                                              'depth': player_1_depth,
                                              'times': player_1_times,
                                              'num_nodes_gen': player_1.num_nodes_generated
                                              },
-                            'second_player': {'id': player_2.__class__,
+                            'second_player': {'id': player_2.__class__.__name__,
                                               'depth': player_2_depth,
                                               'times': player_2_times,
                                               'num_nodes_gen': player_2.num_nodes_generated
@@ -43,19 +44,10 @@ def run_game():
 
 if __name__ == "__main__":
     player_types = [PlayerBFMM, PlayerAB, PlayerMM]
+    results = []
     for player_1_type in player_types:
         for player_2_type in player_types:
-            print(simulate_games(player_1_type, player_2_type))
+            results += simulate_games(player_1_type, player_2_type)
 
-    # ## simulate for mm first
-    # results = simulate_games(PlayerMM, PlayerAB)
-    # print(results)
-    # results_df = pd.DataFrame(results)
-    #
-    # results_df.to_csv('mm_first_results.csv')
-    #
-    # ## simulate for AB first
-    # results = simulate_games(PlayerAB, PlayerMM)
-    # print(results)
-    # results_df = pd.DataFrame(results)
-    # results_df.to_csv('ab_first_results.csv')
+    results_df = json_normalize(results)
+    results_df.to_csv('game_simulation_results.csv')
